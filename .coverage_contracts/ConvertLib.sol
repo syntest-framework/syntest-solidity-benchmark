0873fe18522ce4b2216951db15103a92f771ae30
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2021-02-05 13:46:16 +0100

    Incorporate new CLI

---
+++ .coverage_contracts/ConvertLib.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.7.0;
+
+library ConvertLib{
+function coverage_0xd4644410(bytes32 c__0xd4644410) public pure {}
+
+	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
+	{coverage_0xd4644410(0x210bf933b5916b86e63ba50fb32fb22c2ecff99b9f9f17277b5396f6e83ba26b); /* function */ 
+
+coverage_0xd4644410(0xb3d433b6be80f937f403dbf7c4c9630ed466471a583c19573aa297cf511df3ba); /* line */ 
+		coverage_0xd4644410(0x6d874bec26abef05dc97a5879359f7c292a9d08c0a4bd603931e29f1954ffdb9); /* statement */ 
+return amount * conversionRate;
+	}
+}
