cba0d9278e12c61b06426211f207371699dac7b3
apanichella <a.panichella@tudelft.nl>
2021-03-10 21:24:26 +0100

    Add new contract to the benchmark

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
 const MetaCoin2 = artifacts.require("MetaCoin2");
+const DateTime = artifacts.require("DateTime");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
@@ -8,4 +9,5 @@ module.exports = function(deployer) {
   deployer.deploy(MetaCoin);
   deployer.link(ConvertLib, MetaCoin2);
   deployer.deploy(MetaCoin2);
+  deployer.deploy(DateTime);
 };
