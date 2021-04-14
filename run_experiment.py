0e826277e1ab77673296ecc4a169497de7f286b8
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2021-04-14 22:26:22 +0200

    Add remove flag to the docker execution

--- run_experiment.py
+++ run_experiment.py
@@ -5,4 +5,4 @@ runs_path = "./experiment/runs"
 runs_directories = [f for f in os.listdir(runs_path)]
 
 for i in range(0, len(runs_directories)):
-    subprocess.check_output(['docker', 'run', '-d', '-v', "${PWD}/experiment/runs/" + runs_directories[i] + ':/app/syntest-solidity-benchmark', '-v', '${PWD}/node_modules:/app/syntest-solidity-benchmark/node_modules', 'syntest:experiment'])
+    subprocess.check_output(['docker', 'run', '-d', '--rm', '-v', "${PWD}/experiment/runs/" + runs_directories[i] + ':/app/syntest-solidity-benchmark', '-v', '${PWD}/node_modules:/app/syntest-solidity-benchmark/node_modules', 'syntest:experiment'])
